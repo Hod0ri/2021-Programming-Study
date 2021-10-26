a4bb9d7f36250989b4fafef0eba79e179cd7fe2f
Hod0ri <hodori991004@gmail.com>
2021-10-26 14:18:42 +0900

    [ C# ] - Init Commit

---
+++ College-Lecture/C_Sharp/ParkingAdmin/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAdmin
+{
+    class Car
+    {
+        private int seq;
+        private string carNumber;
+        private DateTime inTime;
+        private DateTime outTime;
+
+        public int Seq { get => seq; set => seq = value; }
+        public string CarNumber { get => carNumber; set => carNumber = value; }
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public void SetOutTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public DateTime GetInTime()
+        {
+            return inTime;
+        }
+        public DateTime GetOutTime()
+        {
+            return outTime;
+        }
+    }
+}

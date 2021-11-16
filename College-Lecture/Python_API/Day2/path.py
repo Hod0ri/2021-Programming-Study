175451300ecb8060c32a470b86df86a583e798e6
Hod0ri <hodori991004@gmail.com>
2021-11-16 23:30:20 +0900

    [ Python ] Check Directory Path

---
+++ College-Lecture/Python_API/Day2/path.py
@@ -0,0 +1,8 @@
+import os,sys,inspect
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+print("Current Directory Path : ", current_dir)
+print("Parent Directory Path : ", parent_dir)
